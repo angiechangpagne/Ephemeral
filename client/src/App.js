37b48ffd61c4161d2bed737e8c3ed2b45c3684c4
angiechangpagne@gmail.com <angiechangpagne@gmail.com>
2019-11-08 20:04:26 -0800

    carousel

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import Carousal from './Carousel'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const  feedItemURL=""
+
+
+
+const App=(props) => {
+    const [imgList,setImgList] =useState([])
+
+
+    useEffect(() => { //analogous to ComponentDidMount
+        axios.get(URL)
+        .then((resp) => {
+            setImgList(resp.data.images)
+
+        }).catch((err) =>{
+            console.log('Unable to feach splashbase url',err)
+        })
+    },[])
+
+
+    return (
+        <div> 
+            <h1> Carousal </h1>
+            {imgList.length===0 && <div> Loading..</div>}
+            {imgList.length>0 && 
+            <Carousal imgList={imgList} img_width={300} img_height={300}
+                visibleImages={3} duration={750} />
+        }
+        </div>
+    )
 }
 
-export default App;
+
+export default App
